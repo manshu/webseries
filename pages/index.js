237f9585c939f315197b578823c19ba3905bf120
Himanshu Batra <himanshu@batra.io>
2020-10-07 13:41:08 -0400

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Index extends Component {
+    render() {
+        return (<div>
+            <h3 className="text-5xl text-green-500">Hello World</h3>
+        </div>)
+    }
+}
+
+export default Index;
